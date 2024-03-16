062b2b1f09e5ce18371c4934fe2ebd6dcb0469a0
Spritaro <k5dot.feb@gmail.com>
2024-03-16 16:36:04 +0900

    use actual serial port for autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,9 +5,10 @@ set -e
 cd /home/nvidia/core_auto_app
 source .venv/bin/activate
 
-# 開発用に仮想シリアルポート使用
-socat -d -d pty,raw,echo=0,link=/tmp/vtty0 pty,raw,echo=0,link=/tmp/vtty1 &
-core_auto_app --robot_port=/tmp/vtty0
+# # 開発用に仮想シリアルポート使用
+# socat -d -d pty,raw,echo=0,link=/tmp/vtty0 pty,raw,echo=0,link=/tmp/vtty1 &
+# core_auto_app --robot_port=/tmp/vtty0
 
-# TODO: 仮想シリアルポートを使用しないアプリケーションの起動
+# 仮想シリアルポートを使用しないアプリケーションの起動
+core_auto_app --robot_port=/tmp/ttyUSB0
 
