bd483e203ceca95fd0ea9825c903a89b96d3b518
Spritaro <k5dot.feb@gmail.com>
2024-03-25 11:19:18 +0900

    fix monitor resolution on startup

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,6 +5,9 @@ set -e
 cd /home/nvidia/core_auto_app
 source .venv/bin/activate
 
+# 解像度変更
+xrandr --output DP-1 --mode 1280x720
+
 # 仮想シリアルポートを使用しないアプリケーションの起動
 core_auto_app --robot_port=/dev/ttyUSB0
 
