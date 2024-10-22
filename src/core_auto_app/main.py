5a53806224aa29dd9ef808224ee7385a8a959574
KuraZuzu <kurazuzukun3@gmail.com>
2024-10-22 15:11:29 +0900

    デフォルトのカメラを"camera_a"に変更

--- src/core_auto_app/main.py
+++ src/core_auto_app/main.py
@@ -21,7 +21,7 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--record_dir",
-        default="/mnt/ssd1",
+        default="/home/nvidia/tmp_record",
         type=str,
         help="directory to record camera log",
     )
