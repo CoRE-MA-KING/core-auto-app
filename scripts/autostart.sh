a464af06b1b35080725bcb9f643bf2a4d3501ec3
KuraZuzu <kurazuzukun3@gmail.com>
2024-12-16 22:36:56 +0900

    カメラをスレッドごとに処理するように変更 (#1)
    * Realsenseを常にスレッドでフレームを取得するように変更し、RealsenseCameraFactoryクラスを削除した
    
    * UsbCameraの実行をスレッド別に行うように変更

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,8 +5,9 @@ set -e
 cd /home/nvidia/core_auto_app
 source .venv/bin/activate
 
-# # 解像度変更
-# xrandr --output DP-0 --mode 1280x720
+# 解像度変更
+# xrandr --output DP-1 --mode 1280x720
+
 # 一度モニタをオフにする
 monitor=$(xrandr --query | grep " connected" | awk '{print $1}')
 sleep 1  # 1秒待機
@@ -14,7 +15,9 @@ echo $monitor
 # モニタをオンにして解像度を設定する（1920x1080, リフレッシュレート60Hz）
 sleep 1  # 1秒待機
 
-xrandr --output "$monitor" --mode 1280x720  --rate 30
+xrandr --output "$monitor" --mode 1280x720
+# xrandr --output "$monitor" --mode 1920x1080
 
 # 仮想シリアルポートを使用しないアプリケーションの起動
-core_auto_app --robot_port=/dev/ttyUSB0
+core_auto_app --robot_port=/dev/ttyACM0
+# core_auto_app
